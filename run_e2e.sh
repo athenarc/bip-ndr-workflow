410dfe2745f6648b515d5a446632de4f78b9b411
Paris Koloveas <pkoloveas@athenarc.gr>
2023-10-03 15:27:04 +0300

    edit file structure

--- run_e2e.sh
+++ run_e2e.sh
@@ -2,32 +2,32 @@
 
 export $(xargs <.env)
 
-./scripts/make_dirs.sh
+./make_dirs.sh
 
-python3 ./src/download_dblp_corpus.py
+python3 download_dblp_corpus.py
 
 gunzip -k ${DBLP_CORPUS_PATH}dblp-${LATEST_DATE}/dblp-${LATEST_DATE}.xml.gz
 
 python3 ./submodules/dblp-to-csv/XMLToCSV.py ${DBLP_CORPUS_PATH}dblp-${LATEST_DATE}/dblp-${LATEST_DATE}.xml ${DBLP_CORPUS_PATH}dblp-${LATEST_DATE}/dblp-*.dtd ${DBLP_CORPUS_PATH}dblp-${LATEST_DATE}/dblp_${LATEST_DATE}.csv
 
 # make options for these two
-# python3 ./src/metadata_extractor.py import_to_mongo
-# python3 ./src/metadata_extractor.py create_download_object
+# python3 metadata_extractor.py import_to_mongo
+# python3 metadata_extractor.py create_download_object
 
-# ./scripts/split_dl_file.sh
+# ./split_dl_file.sh
 
 # java run publication retriever script
 
-# python3 ./src/grobid_pdf2tei.py 1 DocFiles_*
+# python3 grobid_pdf2tei.py 1 DocFiles_*
 
-# python3 ./src/teixml2json_converter.py
+# python3 teixml2json_converter.py
 
 # make options for this
-# python3 ./src/metadata_extractor.py match_key_to_filename
+# python3 metadata_extractor.py match_key_to_filename
 
 # script to merge batches (maybe)
 
-# python3 ./src/dataset_generator.py
+# python3 dataset_generator.py
 
 # ./run_mongoexport.sh
 
--- run_tei2json.sh
+++ run_tei2json.sh
@@ -5,4 +5,4 @@ fi
 
 batch_number="$1"
 
-python3 ./src/teixml2json_converter.py --in_path "/home/pkoloveas/Desktop/dblp_corpus/full_corpus/TEI_XML/References/DocFiles_${batch_number}/" --out_path "/home/pkoloveas/Desktop/dblp_corpus/full_corpus/json_references/DocFiles_${batch_number}/" --yes
+python3 teixml2json_converter.py --in_path "${TEI_PATH}References/DocFiles_${batch_number}/" --out_path "${JSON_PATH}json_references/DocFiles_${batch_number}/" --yes
